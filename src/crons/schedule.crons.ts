341ad28488e8df81a7094bff493cc1760f1a80e3
Rohit Panwar <rohitpanwar9014rp@gmail.com>
2024-07-05 00:47:41 +0530

    cron added

--- src/crons/schedule.crons.ts
+++ src/crons/schedule.crons.ts
@@ -3,7 +3,7 @@ import { KeepAliveCron } from './keepAliveCron';
 
 export class ScheduleCron {
     static async KeepAliveServer() {
-        cron.schedule('*/1 * * * *', KeepAliveCron.FourteenMinReqCron, {
+        cron.schedule('*/14 * * * *', KeepAliveCron.FourteenMinReqCron, {
             scheduled: true,
             timezone: "Asia/Kolkata",
         });
